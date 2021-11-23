29c55e06e73db4198a647333953167f0c388a440
JonPro0 <jzellerhoff05@gmail.com>
2021-11-23 12:51:27 +0100

    initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk')
+const color = require('chalk')
+const { blue } = require('color-name')
+const getNotes = require('./notes.js')
+
+
+const message = getNotes()
+console.log(message)
+
+console.log(chalk.blue('hello pls???'))
+
+
+
+
+
