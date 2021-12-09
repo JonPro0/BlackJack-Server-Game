dcf29fd5871315060f41b2bf6fdd085d164617ce
JonPro0 <zellerhoff05@gmail.com>
2021-12-09 09:27:51 +0100

    add visualisation

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -2,12 +2,14 @@ const socket = io()
 
 socket.on('message', (message) => {
     console.log(message)
+    document.querySelector('#chat').innerHTML += "<p>" + message + "</p>"
 })
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const msg = e.target.elements.message.value
+    document.getElementById('reset').value = ''
 
     
 
