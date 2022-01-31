37a973cb5f9b4935f2f51d988401615722362367
JonPro0 <zellerhoff05@gmail.com>
2022-01-31 09:28:28 +0100

    score & array modified

--- game/public/js/blackjack.js
+++ game/public/js/blackjack.js
@@ -1,18 +1,27 @@
+ 
 
+const cards = [{value : 11, word : "Ace of Club"}, {value : 2, word : "2 of Club"}, {value : 3, word : "3 of Club"}, {value : 4, word : "4 of Club"}, 
+{value : 5, word : "5 of Club"}, {value : 6, word : "6 of Club"}, {value : 7, word : "7 of Club"}, {value : 8, word : "8 of Club"}, {value : 9, word : "9 of Club"},
+{value : 10, word : "10 of Club"}, {value : 10, word : "Jack of Club"}, {value : 10, word : "Queen of Club"}, {value : 10, word : "10 of Club"},
+{value : 11, word : "Ace of Spade"}, {value : 2, word : "2 of Spade"}, {value : 3, word : "3 of Spade"}, {value : 4, word : "4 of Spade"}, 
+{value : 5, word : "5 of Spade"}, {value : 6, word : "6 of Spade"}, {value : 7, word : "7 of Spade"}, {value : 8, word : "8 of Spade"}, {value : 9, word : "9 of Spade"},
+{value : 10, word : "10 of Spade"}, {value : 10, word : "Jack of Spade"}, {value : 10, word : "Queen of Spade"}, {value : 10, word : "10 of Spade"},
+{value : 11, word : "Ace of Heart"}, {value : 2, word : "2 of Heart"}, {value : 3, word : "3 of Heart"}, {value : 4, word : "4 of Heart"}, 
+{value : 5, word : "5 of Heart"}, {value : 6, word : "6 of Heart"}, {value : 7, word : "7 of Heart"}, {value : 8, word : "8 of Heart"}, {value : 9, word : "9 of Heart"},
+{value : 10, word : "10 of Heart"}, {value : 10, word : "Jack of Heart"}, {value : 10, word : "Queen of Heart"}, {value : 10, word : "10 of Heart"},
+{value : 11, word : "Ace of Diamond"}, {value : 2, word : "2 of Diamond"}, {value : 3, word : "3 of Diamond"}, {value : 4, word : "4 of Diamond"}, 
+{value : 5, word : "5 of Diamond"}, {value : 6, word : "6 of Diamond"}, {value : 7, word : "7 of Diamond"}, {value : 8, word : "8 of Diamond"}, {value : 9, word : "9 of Diamond"},
+{value : 10, word : "10 of Diamond"}, {value : 10, word : "Jack of Diamond"}, {value : 10, word : "Queen of Diamond"}, {value : 10, word : "10 of Diamond"}]
 
-const cards = ["Ace of Club", "2 of Club", "3 of Club", "4 of Club", "5 of Club", "6 of Club", "7 of Club", "8 of Club", "9 of Club", "10 of Club", "Jack of Club", "Queen of Club", "King of Club",
-"Ace of Spade", "2 of Spade", "3 of Spade", "4 of Spade", "5 of Spade", "6 of Spade", "7 of Spade", "8 of Spade", "9 of Spade", "10 of Spade", "Jack of Spade", "Queen of Spade", "King of Spade",
-"Ace of Heart", "2 of Heart", "3 of Heart", "4 of Heart", "5 of Heart", "6 of Heart", "7 of Heart", "8 of Heart", "9 of Heart", "10 of Heart", "Jack of Heart", "Queen of Heart", "King of Heart",
-"Ace of Diamond", "2 of Diamond", "3 of Diamond", "4 of Diamond", "5 of Diamond", "6 of Diamond", "7 of Diamond", "8 of Diamond", "9 of Diamond", "10 of Diamond", "Jack of Diamond", "Queen of Diamond", "King of Diamond",
-]
-
-let i = 0
+let score = 0
 
 
 function showCreatedCard() {
-    let card = Math.round((Math.random() * 52))
-    console.log(cards[card])
-    document.querySelector('#player1').innerHTML += `<p>${cards[card]}</p>` 
-    document.querySelector('#playerScore').innerHTML = ++i
+
+    let card = Math.round((Math.random() * 51))
+    console.log(cards[card].word)
+    document.querySelector('#player1').innerHTML += `<p>${cards[card].word}</p>` 
+    score += cards[card].value
+    document.querySelector('#playerScore').innerHTML = score 
     
 }
